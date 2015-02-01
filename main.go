bd868ffdbcde95af2a6e687fec1d48fa216b298c
Ian Kent <email@iankent.co.uk>
2015-02-01 18:20:44 +0000

    Prepare for async

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gorilla/pat"
@@ -114,81 +116,34 @@ func build(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	ts := fmt.Sprintf("%d", time.Now().Unix())
-	dRn := strings.Replace(i.Repo, "/", "+", -1)
-	workParent := "./workdir/" + dRn + "-" + ts
-	workDir := workParent + "/" + i.Repo
+	var wg sync.WaitGroup
+	var rr httptest.ResponseRecorder
 
-	defer cleanup(workParent)
+	wg.Add(1)
+	go func(w httptest.ResponseRecorder) {
+		ts := fmt.Sprintf("%d", time.Now().Unix())
+		dRn := strings.Replace(i.Repo, "/", "+", -1)
+		workParent := "./workdir/" + dRn + "-" + ts
+		workDir := workParent + "/" + i.Repo
 
-	r := &buildResponse{
-		Steps: make([]buildOutput, 0),
-	}
-	var o buildOutput
+		defer cleanup(workParent)
 
-	if len(i.Token) > 0 {
-		if i.Token == "-" {
-			o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "https://github.com/"+i.Repo+".git", workDir)
-		} else {
-			o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "https://"+i.Token+":x-oauth-basic@github.com/"+i.Repo+".git", workDir)
+		r := &buildResponse{
+			Steps: make([]buildOutput, 0),
 		}
-	} else {
-		o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "git@github.com:"+i.Repo+".git", workDir)
-	}
-	if o.Error != nil {
-		log.Printf("Error cloning repo %s: %s", i.Repo, o.Error)
-		r.Steps = append(r.Steps, o)
-		b, err := json.Marshal(&r)
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("Failed to marshal output"))
-			return
-		}
-		w.WriteHeader(200)
-		w.Write(b)
-		return
-	}
-	log.Printf("Cloned repo %s", i.Repo)
+		var o buildOutput
 
-	o.Stdout, o.Stderr, o.Error = runCommand(workDir, i.Env, "git", "checkout", i.Commit)
-	if o.Error != nil {
-		log.Printf("Error checking out commit %s: %s", i.Commit, o.Error)
-		b, err := json.Marshal(&r)
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("Failed to marshal output"))
-			return
-		}
-		w.WriteHeader(200)
-		w.Write(b)
-		return
-	}
-	log.Printf("Checked out %s\n", i.Commit)
-
-	for _, step := range i.Build {
-		var o1 buildOutput
-		o1.Step = step
-		o1.Stdout, o1.Stderr, o1.Error = runCommand(workDir, i.Env, step[0], step[1:]...)
-		r.Steps = append(r.Steps, o1)
-		if o1.Error != nil {
-			log.Printf("Error running build step %s for %s: %s", step, i.Repo, o1.Error)
-			b, err := json.Marshal(&r)
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte("Failed to marshal output"))
-				return
+		if len(i.Token) > 0 {
+			if i.Token == "-" {
+				o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "https://github.com/"+i.Repo+".git", workDir)
+			} else {
+				o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "https://"+i.Token+":x-oauth-basic@github.com/"+i.Repo+".git", workDir)
 			}
-			w.WriteHeader(200)
-			w.Write(b)
-			return
+		} else {
+			o.Stdout, o.Stderr, o.Error = runCommand(".", i.Env, "git", "clone", "git@github.com:"+i.Repo+".git", workDir)
 		}
-		log.Printf("Build step completed for %s", i.Repo)
-	}
-
-	if len(i.Artifacts) > 0 {
-		o.Stdout, o.Stderr, o.Error = runCommand(workDir, i.Env, "find", ".", "-name", i.Artifacts)
 		if o.Error != nil {
-			log.Printf("Error finding artifacts: %s", o.Error)
+			log.Printf("Error cloning repo %s: %s", i.Repo, o.Error)
 			r.Steps = append(r.Steps, o)
 			b, err := json.Marshal(&r)
 			if err != nil {
@@ -200,32 +155,30 @@ func build(w http.ResponseWriter, req *http.Request) {
 			w.Write(b)
 			return
 		}
-		log.Printf("Found artifacts: %s\n", o.Stdout)
+		log.Printf("Cloned repo %s", i.Repo)
 
-		r.Artifacts = make(map[string]string)
-
-		arts := strings.Split(o.Stdout, "\n")
-		for _, art := range arts {
-			art = strings.TrimSpace(art)
-			if len(art) > 0 {
-				b, err := ioutil.ReadFile(workDir + "/" + art)
-				if err != nil {
-					r.Artifacts[art] = fmt.Sprintf("Error reading artifact: %s", err)
-					log.Printf("Failed to add artifact %s: %s", art, err)
-					continue
-				}
-				r.Artifacts[art] = base64.StdEncoding.EncodeToString(b)
-				log.Printf("Added artifact %s (%d bytes)", art, len(b))
+		o.Stdout, o.Stderr, o.Error = runCommand(workDir, i.Env, "git", "checkout", i.Commit)
+		if o.Error != nil {
+			log.Printf("Error checking out commit %s: %s", i.Commit, o.Error)
+			b, err := json.Marshal(&r)
+			if err != nil {
+				w.WriteHeader(500)
+				w.Write([]byte("Failed to marshal output"))
+				return
 			}
+			w.WriteHeader(200)
+			w.Write(b)
+			return
 		}
+		log.Printf("Checked out %s\n", i.Commit)
 
-		for _, step := range i.Publish {
+		for _, step := range i.Build {
 			var o1 buildOutput
 			o1.Step = step
 			o1.Stdout, o1.Stderr, o1.Error = runCommand(workDir, i.Env, step[0], step[1:]...)
 			r.Steps = append(r.Steps, o1)
 			if o1.Error != nil {
-				log.Printf("Error running publish step %s for %s: %s", step, i.Repo, o1.Error)
+				log.Printf("Error running build step %s for %s: %s", step, i.Repo, o1.Error)
 				b, err := json.Marshal(&r)
 				if err != nil {
 					w.WriteHeader(500)
@@ -236,19 +189,84 @@ func build(w http.ResponseWriter, req *http.Request) {
 				w.Write(b)
 				return
 			}
-			log.Printf("Publish step completed for %s", i.Repo)
+			log.Printf("Build step completed for %s", i.Repo)
+		}
+
+		if len(i.Artifacts) > 0 {
+			o.Stdout, o.Stderr, o.Error = runCommand(workDir, i.Env, "find", ".", "-name", i.Artifacts)
+			if o.Error != nil {
+				log.Printf("Error finding artifacts: %s", o.Error)
+				r.Steps = append(r.Steps, o)
+				b, err := json.Marshal(&r)
+				if err != nil {
+					w.WriteHeader(500)
+					w.Write([]byte("Failed to marshal output"))
+					return
+				}
+				w.WriteHeader(200)
+				w.Write(b)
+				return
+			}
+			log.Printf("Found artifacts: %s\n", o.Stdout)
+
+			r.Artifacts = make(map[string]string)
+
+			arts := strings.Split(o.Stdout, "\n")
+			for _, art := range arts {
+				art = strings.TrimSpace(art)
+				if len(art) > 0 {
+					b, err := ioutil.ReadFile(workDir + "/" + art)
+					if err != nil {
+						r.Artifacts[art] = fmt.Sprintf("Error reading artifact: %s", err)
+						log.Printf("Failed to add artifact %s: %s", art, err)
+						continue
+					}
+					r.Artifacts[art] = base64.StdEncoding.EncodeToString(b)
+					log.Printf("Added artifact %s (%d bytes)", art, len(b))
+				}
+			}
+
+			for _, step := range i.Publish {
+				var o1 buildOutput
+				o1.Step = step
+				o1.Stdout, o1.Stderr, o1.Error = runCommand(workDir, i.Env, step[0], step[1:]...)
+				r.Steps = append(r.Steps, o1)
+				if o1.Error != nil {
+					log.Printf("Error running publish step %s for %s: %s", step, i.Repo, o1.Error)
+					b, err := json.Marshal(&r)
+					if err != nil {
+						w.WriteHeader(500)
+						w.Write([]byte("Failed to marshal output"))
+						return
+					}
+					w.WriteHeader(200)
+					w.Write(b)
+					return
+				}
+				log.Printf("Publish step completed for %s", i.Repo)
+			}
+		}
+
+		b, err = json.Marshal(&r)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Failed to marshal output"))
+			return
+		}
+		w.WriteHeader(200)
+		w.Write(b)
+		return
+	}(rr)
+
+	wg.Wait()
+
+	// return response
+	for k, h := range rr.Header() {
+		for _, h1 := range h {
+			w.Header().Add(k, h1)
 		}
 	}
-
-	b, err = json.Marshal(&r)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte("Failed to marshal output"))
-		return
-	}
-	w.WriteHeader(200)
-	w.Write(b)
-	return
+	w.Write(rr.Body.Bytes())
 }
 
 func cleanup(workParent string) {
